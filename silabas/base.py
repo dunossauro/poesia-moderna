415a45936ba4f3a12233830ae741c343ddeb9b1a
z4r4tu5tr4 <mendesxeduardo@gmail.com>
2017-01-26 08:22:24 -0200

    palavras divididas

--- silabas/base.py
+++ silabas/base.py
@@ -1,20 +1,31 @@
 from string import ascii_lowercase as abc
-
 from bs4 import BeautifulSoup
 import urllib
 
-url = 'http://www.portaldalinguaportuguesa.org/index.php?action=syllables&act=list&letter='
+pages = []
+num = 0
+while num < 23000:
+    pages.append(num)
+    num += 100
+
+url = 'http://www.portaldalinguaportuguesa.org/index.php?action=syllables&act=list&letter={}&start={}'
+file = open('divisaosilabica.csv', 'w')
+file.write('palavra,tipo,sílabas')
 
 for letra in abc:
-    r = urllib.request.urlopen(url + letra)
-    data = r.read()
-    soup = BeautifulSoup(data)
-    rows = soup.find(id="rollovertable").findAll("tr")
-    for row in rows:
-        cells = row.findAll("td")
-        if cells:
-            with open('divisaosilabica.csv', 'a') as file:
+    for p in pages:
+        r = urllib.request.urlopen(url.format(letra, p))
+        data = r.read()
+        soup = BeautifulSoup(data)
+        rows = soup.find(id="rollovertable").findAll("tr")
+        for row in rows:
+            cells = row.findAll("td")
+            if cells:
                 palavra = cells[0].find('b').find('a').getText()
                 tipo = cells[0].getText().split("(")[1].split(")")[0]
                 divisaosilabica = cells[1].getText()
-                file.write('{},{},{}'.format(palavra, tipo, divisaosilabica))
+                file.write('{},{},{}'.format(palavra,
+                                             tipo,
+                                             divisaosilabica))
+
+file.close()
